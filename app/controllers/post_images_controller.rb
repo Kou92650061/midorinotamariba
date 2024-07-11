03bc3c46a7e91f60eb91d4b03583d58fce9b33e2
Koji Hori <kojihori0923@gmail.com>
2024-07-11 17:39:19 +0000

    投稿機能2回目

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,6 +2,13 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
 
   def index
     @post_images = PostImage.all
@@ -9,4 +16,11 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:image, :title, :body)
+  end
+  
 end
